6aebf6c6d9dd8bf14bb3862bf0de6ef32d097bb5
Miguel Pecegueiro <miguel.pecegueiro@goconnection.pt>
2020-12-04 18:00:55 +0000

    Add project files.

---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start:
+            Console.WriteLine("Select day (1-25) and Part(1-2) (day.part) : ");
+            string input = Console.ReadLine();
+            try
+            {
+                switch (input)
+                {
+                    case "1.1": Day1.Part1(); break;
+                    case "1.2": Day1.Part2(); break;
+                    case "2.1": Day2.Part1(); break;
+                    case "2.2": Day2.Part2(); break;
+                    case "3.1": Day3.Part1(); break;
+                    case "3.2": Day3.Part2(); break;
+                    case "4.1": Day1.Part1(); break;
+                    case "4.2": Day1.Part1(); break;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            goto Start;
+
+        }
+
+    }
+
+}
